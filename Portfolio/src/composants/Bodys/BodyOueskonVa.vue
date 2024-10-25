b5105a87a39a138c406676f2edaa50fbbcad3d90
Albin <albin.caminade@gmail.com>
2024-10-25 22:26:07 +0200

    feat : ajout ouekonva un peu degueu dnc mis en com

--- Portfolio/src/composants/Bodys/BodyOueskonVa.vue
+++ Portfolio/src/composants/Bodys/BodyOueskonVa.vue
@@ -4,11 +4,88 @@ import Construction from "@/composants/Sous-vues/Construction.vue";
 </script>
 
 <template>
-
-
   <Construction/>
-  <br><br><br><br>
-  <p> Cette page représentera diverses visions du futur, positives ou non, plausible ou non.</p>
+<!--  <h1><p> Cette page représente diverses visions du futur, positives ou non, plausible ou non.</p></h1>-->
+
+<!--  <h3>-->
+<!--    Commençons par un monde positif, un avenir radieux-->
+<!--  </h3>-->
+
+<!--  <strong> L'énergie : </strong>-->
+<!--  <br>-->
+<!--  <p>-->
+<!--    Dans ce monde l'énergie n'est plus un problème, plus besoin de métaux rares, d'énergie fossile, aucune création de-->
+<!--    CO2 pour l'énergie.-->
+<!--    En un mot, la fusion parfaite. Fusion - Fission. Des normes de sécurité sans faille.-->
+<!--  </p>-->
+
+<!--  <strong> Le transport : </strong>-->
+<!--  <br>-->
+<!--  <p>-->
+<!--    Le tram et le train deviennent les uniques moyens de transport à l'échelle mondiale.-->
+<!--    Les routes laissent place à des rails.-->
+<!--    Un travail gigantesque est accomplis pour permettre à l'immense majorité des gens un accès au reste de la-->
+<!--    civilisation.-->
+<!--  </p>-->
+
+<!--  <strong> La nourriture : </strong>-->
+<!--  <br>-->
+<!--  <p> Aucune énergie fossile veut également dire aucun tracteur, l'agriculture intensive est officiellement terminée.-->
+<!--    La permaculture est appliquée à l'échelle mondiale.-->
+<!--    Les fruits et légumes d'anciennes variétés, moins belles mais plus simple à planter refont surface.-->
+<!--    Une resélection complète des souches est faite ce qui permet la suppression complète des pesticides.-->
+<!--    Un travail humain énorme est alors demandé, ce qui nous amène au point suivant.-->
+<!--    La viande devient plus rare, les gens sont priés de faire leurs propre élevage de poules, les animaux deviennent-->
+<!--    chose commune dans les villes.-->
+<!--  </p>-->
+<!--  <strong> La gestion humaine : </strong>-->
+<!--  <br>-->
+<!--  <p>-->
+<!--    Les villes s'allègent, les gens se rapprochent des villages pour avoir leur propre coin de terre.-->
+<!--    L'utopie étant parfaite, les amateurs du capitalisme n'utilisent pas les moyens qui leurs sont propres pour faire-->
+<!--    bouger les masses à leurs avantages.-->
+<!--    Pas de propagande, pas de création de peur massive de pénurie menant à une rébellion armée..-->
+<!--    Chaque citoyen est donc convié 3 semaines par an à travailler la terre, dès 5 ans.-->
+<!--    Les professions liées aux écoles sont donc divisés en deux groupes, les travaux manuels et l'école théorique.-->
+<!--    Rapprocher les humains de la terre leur aide à comprendre la simplicité des choses, et la chance de la technologie.-->
+<!--  </p>-->
+
+<!--  <strong> La technologie : </strong>-->
+<!--  <br>-->
+<!--  <p> Les technologies telles que le téléphone sont très régulées.-->
+<!--    Impossible d'avoir plusieurs téléphones, plusieurs ordinateurs.-->
+<!--    La sobriété est la clé du succès de cette utopie.-->
+<!--    La réparation et l'amélioration des objets connectés existants prend la place de la vente.-->
+<!--    La recherche n'est cependant pas oubliée.-->
+<!--    La compétition n'est simplement plus au centre du sujet, simplement l'amélioration de l'environnement dans lequel-->
+<!--    l'espère évolue.-->
+<!--  </p>-->
+
+<!--  <p>-->
+<!--    <i> Vous me voyez venir gros comme une maison </i>-->
+<!--  </p>-->
+<!--  <strong> Le système économique </strong>-->
+<!--  <br>-->
+<!--  <p>-->
+<!--    Le capitalisme ayant montré ses limites, un système sans monnaie est mis en place.-->
+<!--    L'accomplissement personnel devient l'objectif des humains errant sur cette terre.-->
+<!--    L'existence de pays pauvres et de pays riches n'a plus de sens car l'économie telle que nous la connaissions n'est-->
+<!--    plus.-->
+<!--    Le troc remplace la monnaie mais aucunement de manière quantitative ou qualitative.-->
+<!--    Prendre une bière au bar est encore possible.-->
+<!--    Un barman/cuisinier obtient satisfaction par le plaisir de ses clients.-->
+<!--    Le travail n'est plus obligatoire pour la survie. Mais simplement un droit.-->
+<!--  </p>-->
+
+<!--  <strong>-->
+<!--    Que répondre à ceux qui pensent que quelqu'un qui travaille 12 heures par jour mérite plus que quelqu'un qui ne-->
+<!--    travaille pas ?-->
+<!--  </strong>-->
+<!--  <p>-->
+<!--    Il est vrai que certains travaux sont plus hardu que d'autres.-->
+<!--    Un conducteur de tramway est il l'égal d'un maçon ?-->
+<!--  </p>-->
+
 </template>
 
 <style scoped>
